f6ed6bc324f9c4a5adc063db703eddca0a2faa2d
Shahina <shahinam16@gmail.com>
2024-07-12 03:55:34 +0400

    added some css , pushing package .json

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {Chart, ChartItem} from 'chart.js/auto';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Chart, ChartItem } from 'chart.js/auto';
+
 // Chart.register(ChartDataLabels);
 
 @Component({
@@ -13,59 +13,64 @@ export class DashboardComponent {
   public lineChart1: any;
   public lineChart2: any;
 
+  values = [
+    { icon: 'projects_icon.svg', label: 'Projects', value: 12 },
+    { icon: 'leads_icon.svg', label: 'Leads', value: 12 },
+    { icon: 'bell_icon.svg', label: 'Teams', value: 12 },
+  ];
 
   items = [
     {
-      "name":"Guy Hawkins",
+      "name": "Guy Hawkins",
       "email": "@guy"
     },
     {
-      "name":"Emily Johnson",
+      "name": "Emily Johnson",
       "email": "@emily"
     },
     {
-      "name":"James Smith",
+      "name": "James Smith",
       "email": "@james"
     },
     {
-      "name":"Olivia Jones",
+      "name": "Olivia Jones",
       "email": "@olivia"
     },
     {
-      "name":"Ethan Davis",
+      "name": "Ethan Davis",
       "email": "@ethen"
     },
     {
-      "name":"Sophia Anderson",
+      "name": "Sophia Anderson",
       "email": "@sophia"
     },
     {
-      "name":"Matthew Wilson",
+      "name": "Matthew Wilson",
       "email": "@mathew"
     },
     {
-      "name":"Isabella Miller",
+      "name": "Isabella Miller",
       "email": "@isabella"
     },
   ]
   sortedItems = [...this.items]; // Copy of items to preserve original
 
   sortDirection = 1; // 1 for ascending, -1 for descending
-  sortIcon = '↑'; // Initial ascending icon
+  sortIcon = '↑↓'; // Initial ascending icon
 
-  
+
   chartDataValues: number[] = [25, 30, 25, 20, 20];
-  
-  
+
+
 
   ngOnInit(): void {
     this.createChart();
-    this. createLineChart1()
+    this.createLineChart1()
     this.createLineChart2();
   }
   toggleSort() {
     this.sortDirection = -this.sortDirection; // Toggle between 1 and -1
-    this.sortIcon = this.sortDirection === 1 ? '↑' : '↓'; 
+    this.sortIcon = this.sortDirection === 1 ? '↑' : '↓';
 
     this.sortedItems.sort((a, b) => {
       const nameA = a.name.toUpperCase();
@@ -81,6 +86,7 @@ export class DashboardComponent {
   }
   createChart() {
     // Calculate the sum of chart data values
+
     const sum = this.chartDataValues.reduce((acc, value) => acc + value, 0);
     const labelsWithValues = [
       `${this.chartDataValues[0]} Not Started`,
@@ -99,17 +105,18 @@ export class DashboardComponent {
         const fontSize = (height / 160).toFixed(1);
         ctx.restore();
         ctx.font = fontSize + "em sans-serif";
-        ctx.fillStyle = '#212633'; 
+        ctx.fillStyle = '#212633';
         ctx.textBaseline = "middle";
-  
+
         const text = `${sum}`;
         const textX = Math.round((width - ctx.measureText(text).width) / 4.2);
         const textY = height / 1.75;
-  
+
         ctx.fillText(text, textX, textY);
         ctx.save();
       }
     };
+
 
 
     this.chart = new Chart("MyChart", {
@@ -135,18 +142,18 @@ export class DashboardComponent {
             display: true,
             position: 'right',
             labels: {
-              boxHeight: 12,
-              boxWidth: 12,
+              boxHeight: 8,
+              boxWidth: 8,
               usePointStyle: true,
               padding: 20,
               font: {
-                size: 11, 
+                size: 12,
               },
               color: '#000000'
 
             },
           },
-          
+
           title: {
             display: true,
             text: `Total: ${sum}`,
@@ -154,108 +161,102 @@ export class DashboardComponent {
             padding: 10,
           },
         },
-        
+
       },
-      
-      plugins: [centerTextPlugin] 
+
+      plugins: [centerTextPlugin]
     });
   }
-  
-  
+
+  addHighestPointDot(datasets: any[]) {
+    datasets.forEach((dataset) => {
+      const highestPointIndex = dataset.data.indexOf(Math.max(...dataset.data));
+      dataset.pointRadius = new Array(dataset.data.length).fill(0) as (number | never[]);
+      (dataset.pointRadius as number[])[highestPointIndex] = 5;
+    });
+  }
+
+  datapointLinesPlugin = {
+    id: 'datapointLines',
+    beforeDatasetsDraw(chart: { data?: any; ctx?: any; scales?: any; chartArea?: any; }, args: any, plugins: any) {
+      const { ctx, scales: { x, y }, chartArea: { bottom } } = chart;
+      ctx.save();
+      chart.data.datasets.forEach((dataset: { data: number[]; borderColor: any; }, datasetIndex: any) => {
+        const highestPoint = Math.max(...dataset.data.filter(value => value !== null));
+        const index = dataset.data.indexOf(highestPoint);
+        ctx.beginPath();
+        ctx.strokeStyle = dataset.borderColor; // Use the dataset's border color
+        ctx.lineWidth = 1;
+        ctx.setLineDash([2, 2]); // Add this line to make the line dotted
+        ctx.moveTo(x.getPixelForValue(index), y.getPixelForValue(highestPoint));
+        ctx.lineTo(x.getPixelForValue(index), bottom);
+        ctx.stroke();
+      });
+    }
+  };
+
 
   createLineChart1() {
+
+    const datasets = [
+      {
+        label: "DGHR",
+        data: [null, 0, 100, 200, 300, 380],
+        borderColor: '#A700FF',
+        backgroundColor: '#A700FF',
+        tension: 0.2,// Adjust tension for curved lines (0 is straight lines, 1 is very curved)
+        pointRadius: [], // Remove points
+      },
+      {
+        label: "Mahami",
+        data: [0, 60, 100, 145, 185, 280, 320, 380],
+        borderColor: '#EF4444',
+        backgroundColor: '#EF4444',
+        tension: 0.4,
+        pointRadius: []
+      },
+      {
+        label: "Dubai Court",
+        data: [null, null, null, null, null, 0, 90, 180, 280, 330, 380],
+        borderColor: '#3CD856',
+        backgroundColor: '#3CD856',
+        tension: 0.2,
+        pointRadius: []
+      }
+    ]
+    this.addHighestPointDot(datasets);
+
     this.lineChart1 = new Chart("LineChart1", {
       type: 'line', // Type of chart
-  
+
       data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-          {
-            label: "DGHR",
-            data: [null, 0, 100, 200, 300, 380],
-            borderColor: '#A700FF',
-            backgroundColor: '#A700FF',
-            tension: 0.2,// Adjust tension for curved lines (0 is straight lines, 1 is very curved)
-            pointRadius: 0, // Remove points
-          },
-          {
-            label: "Mahami",
-            data: [0, 60, 100, 145, 185, 280, 320, 380],
-            borderColor: '#EF4444',
-            backgroundColor: '#EF4444',
-            tension: 0.4,
-            pointRadius: 0,
-          },
-          {
-            label: "Dubai Court",
-            data: [null, null, null, null, null,  0, 90, 180, 280, 330, 380],
-            borderColor: '#3CD856',
-            backgroundColor: '#3CD856',
-            tension: 0.2,
-            pointRadius: 0,
-          }
-        ]
+        datasets: datasets
+
       },
       options: {
-         // @ts-ignore
-         annotations: [
-          {
-            type: 'line',
-            mode: 'vertical',
-            scaleID: 'x',
-            value: 'Dec', // Last data point of first dataset
-            borderColor: 'rgba(0, 0, 0, 0.2)',
-            borderDash: [5, 5],
-            label: {
-              content: 'Last data point',
-              enabled: false
-            }
-          },
-          {
-            type: 'line',
-            mode: 'vertical',
-            scaleID: 'x',
-            value: 'Aug', // Last data point of second dataset
-            borderColor: 'rgba(0, 0, 0, 0.2)',
-            borderDash: [5, 5],
-            label: {
-              content: 'Last data point',
-              enabled: false
-            }
-          },
-          {
-            type: 'line',
-            mode: 'vertical',
-            scaleID: 'x',
-            value: 'Nov', // Last data point of third dataset
-            borderColor: 'rgba(0, 0, 0, 0.2)',
-            borderDash: [5, 5],
-            label: {
-              content: 'Last data point',
-              enabled: false
-            }
-          }
-        ],
         aspectRatio: 1,
         layout: {
           padding: {
-            top: 20, 
-            bottom: 20, 
-            left: 20, 
-            right: 20 
+            top: 20,
+            bottom: 20,
+            left: 20,
+            right: 20
           }
         },
         plugins: {
-         
-         
-          
+
           legend: {
             display: true,
             position: 'bottom',
             labels: {
-              boxHeight: 12,
-              boxWidth: 12,
+              boxHeight: 6,
+              boxWidth: 6,
               padding: 20,
+              font: {
+                size: 10,
+              },
+              color: '#464E5F'
             },
           },
           title: {
@@ -265,10 +266,12 @@ export class DashboardComponent {
               size: 15,
               weight: 'bolder'
             },
-            color: '#05004E', 
+            color: '#05004E',
             align: 'start',
           },
-          
+
+
+
         },
         scales: {
           x: {
@@ -280,7 +283,7 @@ export class DashboardComponent {
           y: {
             display: true,
             grid: {
-              display: true 
+              display: true
             },
             beginAtZero: true,
             min: 0,
@@ -290,55 +293,62 @@ export class DashboardComponent {
             }
           }
         },
-        
-      }
+
+      },
+
+      plugins: [this.datapointLinesPlugin]
+
     });
+
   }
-  
-  
+
+
 
   createLineChart2() {
+    const datasets = [
+      {
+        label: "DGHR",
+        data: [310, 300, 220, 140, 130, 180, 300, 310, 240, 180, 140, 120],
+        borderColor: '#A700FF',
+        backgroundColor: '#A700FF',
+        tension: 0.4,
+        pointRadius: 0
+      },
+      {
+        label: "Mahami",
+        data: [250, 240, 200, 110, 150, 300, 370, 350, 280, 210, 160, 120],
+        borderColor: '#EF4444',
+        backgroundColor: '#EF4444',
+        tension: 0.4,
+        pointRadius: []
+      },
+      {
+        label: "Dubai Court",
+        data: [300, 48, 40, 210, 340, 380, 300, 160, 150, 200, 340, 380],
+        borderColor: '#3CD856',
+        backgroundColor: '#3CD856',
+        tension: 0.4,
+        pointRadius: 0
+      }
+    ];
+    this.addHighestPointDot(datasets);
+
     this.lineChart2 = new Chart("LineChart2", {
-      type: 'line', 
-  
+      type: 'line',
+
       data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-          {
-            label: "DGHR",
-            data: [310, 300, 220, 140, 130, 180, 300, 310, 240, 180, 140, 120],
-            borderColor: '#A700FF',
-            backgroundColor: '#A700FF',
-            tension: 0.4,
-            pointRadius: 0 
-          },
-          {
-            label: "Mahami",
-            data: [250, 240, 200, 110, 150, 300, 370, 350, 280, 210, 160, 120],
-            borderColor: '#EF4444',
-            backgroundColor: '#EF4444',
-            tension: 0.4,
-            pointRadius: 0 
-          },
-          {
-            label: "Dubai Court",
-            data: [300, 50, 40, 210, 340, 390, 300, 160, 150, 200, 340, 390],
-            borderColor: '#3CD856',
-            backgroundColor: '#3CD856',
-            tension: 0.4,
-            pointRadius: 0 
-          }
-        ]
+        datasets: datasets
       },
       options: {
         aspectRatio: 1,
-        
+
         layout: {
           padding: {
-            top: 20, 
-            bottom: 20, 
-            left: 20, 
-            right: 20 
+            top: 20,
+            bottom: 20,
+            left: 20,
+            right: 20
           }
         },
         plugins: {
@@ -346,9 +356,13 @@ export class DashboardComponent {
             display: true,
             position: 'bottom',
             labels: {
-              boxHeight: 12,
-              boxWidth: 12,
+              boxHeight: 6,
+              boxWidth: 6,
               padding: 20,
+              font: {
+                size: 10,
+              },
+              color: '#464E5F'
             },
           },
           title: {
@@ -356,19 +370,19 @@ export class DashboardComponent {
             text: `Completed Tasks`,
             font: {
               size: 15,
-              weight: 'bold' 
+              weight: 'bold'
             },
-            color: '#05004E', 
+            color: '#05004E',
             align: 'start',
-          
+
           },
         },
         scales: {
           x: {
             display: true,
             grid: {
-              display: false 
-            }
+              display: false
+            },
           },
           y: {
             display: true,
@@ -383,7 +397,9 @@ export class DashboardComponent {
             }
           }
         }
-      }
+      },
+      plugins: [this.datapointLinesPlugin]
+
     });
   }
 }
