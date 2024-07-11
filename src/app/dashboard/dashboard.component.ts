b67e42e021d58346fe77120999957a2090d26ebb
Shahina <shahinam16@gmail.com>
2024-07-11 16:24:36 +0400

    styling of charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import Chart from 'chart.js/auto';
-import 'chartjs-plugin-datalabels';
+import {Chart, ChartItem} from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+// Chart.register(ChartDataLabels);
 
 @Component({
   selector: 'app-dashboard',
@@ -9,8 +10,9 @@ import 'chartjs-plugin-datalabels';
 })
 export class DashboardComponent {
   public chart: any;
-  public lineChart: any;
   public lineChart1: any;
+  public lineChart2: any;
+
 
   items = [
     {
@@ -52,13 +54,14 @@ export class DashboardComponent {
   sortIcon = '↑'; // Initial ascending icon
 
   
-  chartDataValues: number[] = [25, 30, 25, 20];
+  chartDataValues: number[] = [25, 30, 25, 20, 20];
+  
   
 
   ngOnInit(): void {
     this.createChart();
-    this.createLineChart();
     this. createLineChart1()
+    this.createLineChart2();
   }
   toggleSort() {
     this.sortDirection = -this.sortDirection; // Toggle between 1 and -1
@@ -80,18 +83,26 @@ export class DashboardComponent {
   createChart() {
     // Calculate the sum of chart data values
     const sum = this.chartDataValues.reduce((acc, value) => acc + value, 0);
+    const labelsWithValues = [
+      `Not Started ${this.chartDataValues[0]}`,
+      `On Hold ${this.chartDataValues[1]}`,
+      `Submitted ${this.chartDataValues[2]}`,
+      `In Progress ${this.chartDataValues[3]}`,
+      `Completed ${this.chartDataValues[4]}`
+    ];
   
     this.chart = new Chart("MyChart", {
       type: 'doughnut',
       data: {
-        labels: ['On Hold', 'Submitted', 'In Progress', 'Not Started'],
+        labels: labelsWithValues,
         datasets: [{
           data: this.chartDataValues,
           backgroundColor: [
-            'red',
-            'blue',
-            'orange',
-            'green',
+            '#5EE173',
+            '#F93535',
+            '#356CF9',
+            '#F9AB35',
+            '#c7892c',
           ],
           hoverOffset: 4
         }],
@@ -107,8 +118,14 @@ export class DashboardComponent {
               boxWidth: 12,
               usePointStyle: true,
               padding: 20,
+              font: {
+                size: 11, 
+              },
+              color: '#000000'
+
             },
           },
+          
           title: {
             display: true,
             text: `Total: ${sum}`, // Display total sum in chart title
@@ -116,101 +133,172 @@ export class DashboardComponent {
             padding: 10,
           },
         },
+        
       },
     });
   }
   
   
 
-  createLineChart(){
+  createLineChart1() {
+    this.lineChart1 = new Chart("LineChart1", {
+      type: 'line', // Type of chart
   
-    this.lineChart = new Chart("MyLineChart", {
-      type: 'line', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['Jan', 'Feb', 'Mar','Apr', 'May', 'June',
-								 'July', 'Aug', 'Sep','Oct', 'Nov', 'Dec'], 
-	       datasets: [
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
           {
             label: "DGHR",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'violet'
+            data: [null, 0, 100, 200, 300, 380],
+            borderColor: '#A700FF',
+            backgroundColor: '#A700FF',
+            tension: 0.4,// Adjust tension for curved lines (0 is straight lines, 1 is very curved)
+            pointRadius: 0 // Remove points
           },
           {
             label: "Mahami",
-            data: ['0', '98', '100', '110', '200',
-									 '250', '538', '541'],
-            backgroundColor: 'red'
-          }  ,
+            data: [0, 60, 100, 145, 185, 280, 320, 380],
+            borderColor: '#EF4444',
+            backgroundColor: '#EF4444',
+            tension: 0.4,
+            pointRadius: 0 // Remove points
+          },
           {
             label: "Dubai Court",
-            data: ['17', '19', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
+            data: [null, null, null, null, null,  0, 90, 180, 280, 330, 380],
+            borderColor: '#3CD856',
+            backgroundColor: '#3CD856',
+            tension: 0.4,
+            pointRadius: 0 // Remove points
+          }
         ]
       },
       options: {
-        aspectRatio:1,
+        aspectRatio: 1,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              boxHeight: 12,
+              boxWidth: 12,
+              padding: 20,
+            },
+          },
+          title: {
+            display: true,
+            text: `Completed Tasks`,
+            font: {
+              size: 15,
+              weight: 'bolder'
+            },
+            color: '#05004E', 
+          
+          },
+        },
         scales: {
+          x: {
+            display: true,
+            grid: {
+              display: false // Hide x axis gridlines
+            }
+          },
           y: {
+            display: true,
+            grid: {
+              display: true // Hide y axis gridlines
+            },
             beginAtZero: true,
             min: 0,
-            max: 1000,
+            max: 400,
             ticks: {
               stepSize: 100
             }
           }
         }
       }
-      
     });
   }
-
-  createLineChart1(){
   
-    this.lineChart1 = new Chart("LineChart", {
-      type: 'line', //this denotes tha type of chart
+  
 
-      data: {// values on X-Axis
-        labels: ['Jan', 'Feb', 'Mar','Apr', 'May', 'June',
-								 'July', 'Aug', 'Sep','Oct', 'Nov', 'Dec'], 
-	       datasets: [
+  createLineChart2() {
+    this.lineChart2 = new Chart("LineChart2", {
+      type: 'line', // Type of chart
+  
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
           {
             label: "DGHR",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'violet'
+            data: [310, 300, 220, 140, 130, 180, 300, 310, 240, 180, 140, 120],
+            borderColor: '#A700FF',
+            backgroundColor: '#A700FF',
+            tension: 0.4,// Adjust tension for curved lines (0 is straight lines, 1 is very curved)
+            pointRadius: 0 // Remove points
           },
           {
             label: "Mahami",
-            data: ['0', '98', '100', '110', '200',
-									 '250', '538', '541'],
-            backgroundColor: 'red'
-          }  ,
+            data: [250, 240, 200, 110, 150, 300, 370, 350, 280, 210, 160, 120],
+            borderColor: '#EF4444',
+            backgroundColor: '#EF4444',
+            tension: 0.4,
+            pointRadius: 0 // Remove points
+          },
           {
             label: "Dubai Court",
-            data: ['17', '19', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
+            data: [300, 50, 40, 210, 340, 390, 300, 160, 150, 200, 340, 390],
+            borderColor: '#3CD856',
+            backgroundColor: '#3CD856',
+            tension: 0.4,
+            pointRadius: 0 // Remove points
+          }
         ]
       },
       options: {
-        aspectRatio:1,
+        aspectRatio: 1,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              boxHeight: 12,
+              boxWidth: 12,
+              padding: 20,
+            },
+          },
+          title: {
+            display: true,
+            text: `Completed Tasks`,
+            font: {
+              size: 15,
+              weight: 'bold' 
+            },
+            color: '#05004E', 
+          
+          },
+        },
         scales: {
+          x: {
+            display: true,
+            grid: {
+              display: false // Hide x axis gridlines
+            }
+          },
           y: {
+            display: true,
+            grid: {
+              display: true // Hide y axis gridlines
+            },
             beginAtZero: true,
             min: 0,
-            max: 1000,
+            max: 400,
             ticks: {
               stepSize: 100
             }
           }
         }
       }
-      
     });
   }
 }
